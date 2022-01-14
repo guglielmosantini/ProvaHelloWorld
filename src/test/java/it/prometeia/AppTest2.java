6e3f61ca7d7cb686949e19700b7b32cd5d9dfc74
Guglielmo Santini <santinig@prometeia.lan>
2022-01-14 12:22:25 +0100

    first commit

---
+++ src/test/java/it/prometeia/AppTest2.java
@@ -0,0 +1,14 @@
+package it.prometeia;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AppTest2 {
+
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+}
