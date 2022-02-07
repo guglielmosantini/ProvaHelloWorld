37a6c4c6450162da8e1acb70a58bf15a86b03b15
PROMLAN\santinig <guglielmo.santini@prometeia.com>
2022-02-07 10:40:14 +0100

    Revert "Update App.java"
    This reverts commit 70b0905cd4426d80268593dc4298358c55f307ef.

--- src/main/java/it/prometeia/App.java
+++ src/main/java/it/prometeia/App.java
@@ -1,31 +1,13 @@
 package it.prometeia;
 
-import java.util.Scanner;
-
-import org.apache.commons.lang3.StringUtils;
-
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    private static Scanner sca;
-
-	public static void main( String[] args )
+    public static void main( String[] args )
     {
-        sca = new Scanner(System.in);
         System.out.println( "Hello World!" );
-        String string= sca.next();
-        if (!StringUtils.isEmpty(string)) {
-			System.out.println("Hello "+string);
-		}else {
-			System.out.println("NADA");
-		}
-        
-        
-        
-        
-        
     }
 }
