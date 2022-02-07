1c1ef5bcb1d55c9ca375fd3af63a3df8abdcb985
PROMLAN\santinig <guglielmo.santini@prometeia.com>
2022-02-07 10:17:40 +0100

    update something

--- src/main/java/it/prometeia/App.java
+++ src/main/java/it/prometeia/App.java
@@ -1,13 +1,31 @@
 package it.prometeia;
 
+import java.util.Scanner;
+
+import org.apache.commons.lang3.StringUtils;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    //private static Scanner sca;
+
+	public static void main( String[] args )
     {
+        Scanner sca = new Scanner(System.in);
         System.out.println( "Hello World!" );
+        String string= sca.next();
+        if (!StringUtils.isEmpty(string)) {
+			System.out.println("Hello "+string);
+		}else {
+			System.out.println("NADA");
+		}
+        
+        
+        
+        
+        
     }
 }
