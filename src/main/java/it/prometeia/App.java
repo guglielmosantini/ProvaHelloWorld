991eb87deca599d5bb1481626720c6ffafc80e8a
PROMLAN\santinig <guglielmo.santini@prometeia.com>
2022-02-07 10:36:03 +0100

    fix app.java

--- src/main/java/it/prometeia/App.java
+++ src/main/java/it/prometeia/App.java
@@ -10,19 +10,15 @@ import org.apache.commons.lang3.StringUtils;
  */
 public class App 
 {
-<<<<<<< HEAD
     //private static Scanner sca;
 
-	public static void main( String[] args )
-    {
-        Scanner sca = new Scanner(System.in);
-=======
+	
     private static Scanner sca;
 
 	public static void main( String[] args )
     {
+		
         sca = new Scanner(System.in);
->>>>>>> b45f238c5358ef5e06815d0ef476f2ba889a8139
         System.out.println( "Hello World!" );
         String string= sca.next();
         if (!StringUtils.isEmpty(string)) {
